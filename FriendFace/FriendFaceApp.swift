fd9a847c1dd33dca79aad36392450e1563c82876
Carson <carsongro@gmail.com>
2022-06-18 13:37:21 -0600

    Initial Commit

---
+++ FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Carson Gross on 6/18/22.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
